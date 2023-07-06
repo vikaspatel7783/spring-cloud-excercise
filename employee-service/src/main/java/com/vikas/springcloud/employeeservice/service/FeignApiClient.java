a43f3e16d4a26fed5934357049f28c77cb515d96
Vikas Patel <vikaspatel7783@gmail.com>
2023-07-06 09:35:53 +0530

    Dynamic service discovery added through Load balancing

--- employee-service/src/main/java/com/vikas/springcloud/employeeservice/service/FeignApiClient.java
+++ employee-service/src/main/java/com/vikas/springcloud/employeeservice/service/FeignApiClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080/api", value = "department-service-feign-client")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface FeignApiClient {
 
-    @GetMapping("departments/{department-code}")
+    @GetMapping("api/departments/{department-code}")
     DepartmentDto findByDepartmentCode(@PathVariable("department-code") String departmentCode);
 }
